051b04635b46d962af889187b16243345fbdef72
Logan Cole <emtlogancole@gmail.com>
2024-04-04 06:27:48 -0500

    create conditional running of the file for testing

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -9,14 +9,17 @@ class Chisel
 end
 # Let there be a markdown parser
 # read the md file
-markdown_file = ARGV[0]
-html_file = ARGV[1]
-markdown = File.read(markdown_file)
-# make it html
-html = Chisel.new(markdown).to_html
-# write it to the html file
-File.write(html_file, html)
-# print summary
-markdown_lines = markdown_file.lines
-html_lines     = html_file.html.lines 
-p "Converted my_input.markdown (#{markdown_lines} lines) to my_output.html (#{html_lines} lines)"
+program_running? = ($PROGRAM_NAME == __FILE__)
+if program_running?
+  markdown_file = ARGV[0]
+  html_file = ARGV[1]
+  markdown = File.read(markdown_file)
+  # make it html
+  html = Chisel.new(markdown).to_html
+  # write it to the html file
+  File.write(html_file, html)
+  # print summary
+  markdown_lines = markdown.lines.count
+  html_lines     = html.lines.count
+  p "Converted my_input.markdown (#{markdown_lines} lines) to my_output.html (#{html_lines} lines)"
+end
